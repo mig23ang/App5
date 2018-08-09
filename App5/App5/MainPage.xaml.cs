2970ded12bb818a1787f204b4cbba4364d4c740b
mig23ang <41449316+mig23ang@users.noreply.github.com>
2018-08-09 16:34:48 -0500

    Initial commit

---
+++ App5/App5/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App5
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        async void Handle_Navigated(object sender, Xamarin.Forms.WebNavigatedEventArgs e)
+        {
+            await DisplayAlert("Completado",
+                               "Se ha completado la carga de la pagina",
+                               "OK");
+        }
+    }
+}
---
+++ App5/App5/Page2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page2 : ContentPage
+	{
+		public Page2 ()
+		{
+			InitializeComponent ();
+		}
+        void CambiarColor(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            var rojo = slaRojo.Value;
+            var azul = slaAzul.Value;
+            var verde = slaVerde.Value;
+            Color bgColor = new Color(rojo, azul, verde);
+            boxColor.BackgroundColor = bgColor;
+            lbdDisplay.Text = ColorToHex(bgColor);
+        }
+        private string ColorToHex(Color color)
+        {
+            int rojo = (int)(color.R * 255);
+            int verde = (int)(color.R * 255);
+            int azul = (int)(color.R * 255);
+            int alpha = (int)(color.R * 255);
+            return $"#{rojo:X2}{verde:X2}{azul:X2}{alpha:X2}";
+        }
+
+    }
+}
